524a7d639daab95b0ad35a7a69eea4c97045c323
Litiano <litiano.moura@yahoo.com.br>
2026-03-21 20:16:50 -0300

    fix: collector

--- http-server/infra/http/http-request.go
+++ http-server/infra/http/http-request.go
@@ -7,16 +7,21 @@ import (
 	"io"
 	"net/http"
 	"time"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/propagation"
 )
 
-func RequestWithTimeout(timeout time.Duration, method, url string, body io.Reader) (*http.Response, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+func RequestWithTimeout(ctx context.Context, timeout time.Duration, method, url string, body io.Reader, requestId string) (*http.Response, error) {
+	ctx, cancel := context.WithTimeout(ctx, timeout)
 	defer cancel()
 	req, err := http.NewRequestWithContext(ctx, method, url, body)
 	if err != nil {
 		fmt.Println("Error: ", err)
 		return nil, err
 	}
+	otel.GetTextMapPropagator().Inject(ctx, propagation.HeaderCarrier(req.Header))
+	//req.Header.Set("X-Request-Id", requestId)
 
 	resp, err := http.DefaultClient.Do(req)
 	if resp != nil && resp.StatusCode != http.StatusOK {
